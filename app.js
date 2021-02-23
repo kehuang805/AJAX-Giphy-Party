ebde25102c9ed47907b824c9c28deb3fea995458
Kevin Huang <kehuang805@gmail.com>
2021-02-23 15:40:17 -0800

    main function written

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log("Let's get this party started!");
+
+$(mainFunction);
+
+function mainFunction() {
+    $("#submit").on("click", searchAJAX)
+}
+
+async function searchAJAX(){
+    let search = $("search-term").val();
+    let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${search}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+    console.log(response);
+    $("search-term").empty();
+    showResponse(response);
+}
+
+function showResponse(res){
+    $("gifs").append(res.data);
+}
+

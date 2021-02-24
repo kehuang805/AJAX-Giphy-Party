a448d10c8197f76b94c73ae4d50801ae6abfb4ff
m-zz <marcusyzhu@gmail.com>
2021-02-23 16:48:25 -0800

    completed parts two and three, final

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ console.log("Let's get this party started!");
 $(mainFunction);
 
 function mainFunction() {
-    $("#submit").on("click", searchAJAX)
+    $("#form").on("submit", searchAJAX);
+    
+    $("#remove").on("click", () => {
+        $(".gifs").empty();
+    });
 }
 
-async function searchAJAX(){
-    let search = $("search-term").val();
+async function searchAJAX(evt){
+    evt.preventDefault();
+    let search = $("#search-term").val();
     let response = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${search}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
     console.log(response);
-    $("search-term").empty();
+
+    $("#search-term").val("");
     showResponse(response);
 }
 
 function showResponse(res){
-    $("gifs").append(res.data);
+    let index = Math.floor(Math.random() * res.data.data.length);
+    let gifURL = res.data.data[index].images.original.url;
+    $(".gifs").append($(`<div class=gifGallery><img src="${gifURL}"></div>`));
 }
-
